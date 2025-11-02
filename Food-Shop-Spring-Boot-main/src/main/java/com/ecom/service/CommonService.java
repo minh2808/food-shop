d444ae8bec6e4540a00badfed07f850de12ca680
Pham Ngoc minh <minhphamngoc808@gmail.com>
2025-11-02 18:08:26 +0700

    update code

---
+++ Food-Shop-Spring-Boot-main/src/main/java/com/ecom/service/CommonService.java
@@ -0,0 +1,7 @@
+package com.ecom.service;
+
+public interface CommonService {
+
+	public String formatVnd(Double price) ;
+	public void removeSessionMessage();
+}
---
+++ Food-Shop-Spring-Boot-main/src/main/java/com/ecom/service/CommonServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ecom.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+@Service
+public class CommonServiceImpl implements CommonService {
+
+		@Override
+		public String formatVnd(Double price) {
+			if (price == null) return "0";
+			NumberFormat formatter = NumberFormat.getInstance(new Locale("vi", "VN"));
+			return formatter.format(price);
+		}
+
+
+		@Override
+		public void removeSessionMessage() {
+			HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.getRequestAttributes()))
+					.getRequest();
+			HttpSession session = request.getSession();
+			session.removeAttribute("succMsg");
+			session.removeAttribute("errorMsg");
+		}
+	}
+
+
