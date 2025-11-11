58345af48434184c59a4985b46edaa7fc9ab60db
khiem <khiemmanhtran@gmail.com>
2025-11-11 18:27:11 +0700

    sửa lại model

--- src/main/java/com/ecom/model/OrderAddress.java
+++ src/main/java/com/ecom/model/OrderAddress.java
@@ -1,19 +1,10 @@
 package com.ecom.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class OrderAddress {
 
 	@Id
@@ -23,67 +14,13 @@ public class OrderAddress {
 
 	private String fullName;
 
-
 	private String email;
 
 	private String mobileNo;
 
 	private String address;
 
-
 	private String cityOrProvince;
-
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getMobileNo() {
-        return mobileNo;
-    }
-
-    public void setMobileNo(String mobileNo) {
-        this.mobileNo = mobileNo;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-
-    // fixed getter name to match field 'cityOrProvince' (camel-case O)
-    public String getCityOrProvince() {
-        return cityOrProvince;
-    }
-
-    public void setCityOrProvince(String cityOrProvince) {
-        this.cityOrProvince = cityOrProvince;
-    }
 }
 
 
--- src/main/java/com/ecom/model/OrderRequest.java
+++ src/main/java/com/ecom/model/OrderRequest.java
@@ -1,14 +1,11 @@
 package com.ecom.model;
 
 import lombok.Data;
-import lombok.ToString;
 
-@ToString
 @Data
 public class OrderRequest {
 
 	private String fullName;
-
 
 	private String email;
 
@@ -16,60 +13,7 @@ public class OrderRequest {
 
 	private String address;
 
-
 	private String cityOrProvince;
 
 	private String paymentType;
-	
-	public String getFullName() {
-		return fullName;
-	}
-
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getMobileNo() {
-		return mobileNo;
-	}
-
-	public void setMobileNo(String mobileNo) {
-		this.mobileNo = mobileNo;
-	}
-
-	public String getAddress() {
-		return address;
-	}
-
-	public void setAddress(String address) {
-		this.address = address;
-	}
-
-
-	public String getCityOrProvince() {
-		return cityOrProvince;
-	}
-
-	public void setCityOrProvince(String cityOrProvince) {
-		this.cityOrProvince = cityOrProvince;
-	}
-
-	public String getPaymentType() {
-		return paymentType;
-	}
-
-	public void setPaymentType(String paymentType) {
-		this.paymentType = paymentType;
-	}
-
-	
 }
